a430f16c75da5c84837a1d61e211bd035b4e055f
Shammel Lee <holler@shammellee.com>
2017-05-08 03:48:38 -0400

    Add filter by name

--- exercise.js
+++ exercise.js
@@ -4,23 +4,51 @@ var app = {};
 
 app.start = function(records)
 {
-  var max_record_count     = 30
-      ,first_records       = []
-      ,today               = new Date()
-      ,record_list         = get_by_id('record_list')
-      ,list_item_container = document.createDocumentFragment()
-      ,months              = 'Jan:Feb:Mar:Apr:May:Jun:Jul:Aug:Sep:Oct:Nov:Dec'.split(':')
-      ,selected_list_item  = null
-      ,filter_expired_btn  = get_by_id('button__filter_expired')
-      ,theme_toggle_btn    = get_by_id('button__theme_toggle')
-      ,dea_number_toggle   = get_by_id('checkbox__dea_number_toggle')
-      ,npi_toggle          = get_by_id('checkbox__npi_toggle')
-      ,provider_id_toggle  = get_by_id('checkbox__provider_id_toggle')
-      ,dea_number_tables   = []
-      ,npi_tables          = []
-      ,provider_id_tables  = []
+  var max_record_count          = 30
+      ,first_records            = []
+      ,record_elements          = []
+      ,valid_records            = []
+      ,expired_records          = []
+      ,today                    = new Date()
+      ,record_list              = get_by_id('record_list')
+      ,record_container         = document.createDocumentFragment()
+      ,months                   = 'Jan:Feb:Mar:Apr:May:Jun:Jul:Aug:Sep:Oct:Nov:Dec'.split(':')
+      ,selected_record          = null
+      ,selected_item_class      = 'record__selected'
+      ,detail_query_class       = '.record_container__detail'
+      ,detail_hidden_class      = 'record_container__detail__hidden'
+      ,hidden_class             = 'hidden'
+      ,invisible_class          = 'invisible'
+      ,expired_records_visible  = true
+      ,filter_expired_btn       = get_by_id('button__filter_expired')
+      ,filter_expired_label     = filter_expired_btn.querySelector('.button_label__filter_expired')
+      ,filter_expired_show_text = 'Include expired'
+      ,filter_expired_hide_text = 'Exclude expired'
+      ,dark_theme               = {class_name:'theme__dark',text:'Dark'}
+      ,light_theme              = {class_name:'theme__light',text:'Light'}
+      ,theme_toggle_btn         = get_by_id('button__theme_toggle')
+      ,dea_number_toggle        = get_by_id('checkbox__dea_number_toggle')
+      ,npi_toggle               = get_by_id('checkbox__npi_toggle')
+      ,provider_id_toggle       = get_by_id('checkbox__provider_id_toggle')
+      ,dea_number_tables        = []
+      ,npi_tables               = []
+      ,provider_id_tables       = []
+      ,record_filter            = get_by_id('record_filter')
+      ,record_filter_interval   = 150  // milliseconds
+      ,no_records_message       = get_by_id('message__no_records');
       ;
 
+  if(expired_records_visible)
+  {
+    filter_expired_label.textContent = filter_expired_hide_text;
+  }else
+  {
+    filter_expired_label.textContent = filter_expired_show_text;
+  }
+
+  // -------
+  // HELPERS
+  // -------
 
   function get_by_id(id)
   {
@@ -33,7 +61,7 @@ app.start = function(records)
   // ---------------
 
   // Parse expiration date
-  for(var i = 0,len = records.length;i < len;i++)
+  for(var i = 0,size = records.length;i < size;i++)
   {
     var date_parts = records[i].expiration_date.split('-')
         ,exp_year  = date_parts[0]
@@ -65,60 +93,56 @@ app.start = function(records)
   // CONFIGURE UI
   // ------------
 
-  function toggle_expired(event)
+  function toggle_expired_records(event)
   {
     var filter_expired_label = event.currentTarget.querySelector('.button_label__filter_expired')
         ,filter_label_text   = filter_expired_label.textContent
-        ,expired_records     = record_list.querySelectorAll('.list_item__expired')
-        ,detail_query_class  = '.list_item_container__detail'
-        ,detail_hidden_class = 'list_item_container__detail__hidden'
         ;
 
-    if(filter_label_text == 'Show Expired')
+    if(!expired_records_visible)
     {
       // Show expired records
-      for(var i = 0,len = expired_records.length;i < len;i++)
+      for(var i = 0,size = expired_records.length;i < size;i++)
       {
-        var detail_container = expired_records[i].querySelector(detail_query_class);
+        var detail_container = expired_records[i]
+                                 .querySelector(detail_query_class);
 
         // Deselect expired items, if any
-        expired_records[i].classList.remove('list_item__selected');
-        detail_container.classList.add(detail_hidden_class);
-
-        expired_records[i].classList.remove('hidden');
+        deselect_record(expired_records[i]);
+        show(expired_records[i]);
       }
 
-      filter_expired_label.textContent = 'Hide Expired';
+      filter_expired_label.textContent = filter_expired_hide_text;
+      expired_records_visible          = true;
+      filter_visible_records();
     }else
     {
       // Hide expired records
-      for(var i = 0,len = expired_records.length;i < len;i++)
+      for(var i = 0,size = expired_records.length;i < size;i++)
       {
-        expired_records[i].classList.add('hidden');
+        hide(expired_records[i]);
       }
 
-      filter_expired_label.textContent = 'Show Expired';
+      filter_expired_label.textContent = filter_expired_show_text;
+      expired_records_visible          = false;
     }
-
   }
 
   function toggle_theme(event)
   {
-    var theme_root   = document.body
-        ,dark_theme  = {class_name:'theme__dark',text:'Dark'}
-        ,light_theme = {class_name:'theme__light',text:'Light'}
-        ,button      = event.currentTarget;
+    var theme_root = document.body
+        ,button    = event.currentTarget;
         ;
 
     if(theme_root.classList.contains(dark_theme.class_name))
     {
-      theme_root.classList.remove(dark_theme.class_name);
-      theme_root.classList.add(light_theme.class_name);
+      remove_class(theme_root,dark_theme.class_name);
+      add_class(theme_root,light_theme.class_name);
       button.textContent = 'Show ' + dark_theme.text + ' Theme';
     }else
     {
-      theme_root.classList.remove(light_theme.class_name);
-      theme_root.classList.add(dark_theme.class_name);
+      remove_class(theme_root,light_theme.class_name);
+      add_class(theme_root,dark_theme.class_name);
       button.textContent = 'Show ' + light_theme.text + ' Theme';
     }
   }
@@ -131,14 +155,161 @@ app.start = function(records)
     {
       for(var i = 0; i < size; i++)
       {
-        node_array[i].classList.remove(class_name);
+        remove_class(node_array[i],class_name);
       }
     }else
     {
       for(var i = 0; i < size; i++)
       {
-        node_array[i].classList.add(class_name);
+        add_class(node_array[i],class_name);
       }
+    }
+  }
+
+  function record_filter_value_changed(event)
+  {
+    window.setTimeout(function()
+    {
+      filter_visible_records();
+
+      record_filter.addEventListener(
+        'input'
+        ,record_filter_value_changed
+        ,{once:true,capture:false}
+      );
+    },record_filter_interval);
+  }
+
+  function filter_visible_records()
+  {
+    var filter_term = record_filter.value;
+
+    deselect_record(selected_record);
+
+    if(expired_records_visible)
+    {
+      filter_records(record_elements,filter_term);
+    }else
+    {
+      filter_records(valid_records,filter_term);
+    }
+  }
+
+  function filter_records(records,filter_term)
+  {
+    var filter_regex          = new RegExp(filter_term,'i')
+        ,matched_record_count = 0
+        ;
+
+    conceal(no_records_message);
+
+    for(var i = 0,size = records.length; i < size; i++)
+    {
+      var record_title = records[i].getAttribute('title');
+
+      hide(records[i]);
+
+      if(filter_regex.test(record_title))
+      {
+        show(records[i]);
+        matched_record_count++;
+      }
+    }
+
+    matched_record_count === 0 && reveal(no_records_message);
+  }
+
+  function show(element)
+  {
+    remove_class(element,hidden_class);
+  }
+
+  function hide(element)
+  {
+    add_class(element,hidden_class);
+  }
+
+  function reveal(element)
+  {
+    remove_class(element,invisible_class);
+  }
+
+  function conceal(element)
+  {
+    add_class(element,invisible_class);
+  }
+
+  function add_class(element)
+  {
+    var class_names = [].slice.call(arguments,1);
+
+    for(var i = 0,size = class_names.length; i < size; i++)
+    {
+      element.classList.add(class_names[i]);
+    }
+  }
+
+  function remove_class(element)
+  {
+    var class_names = [].slice.call(arguments,1);
+
+    for(var i = 0,size = class_names.length; i < size; i++)
+    {
+      element.classList.remove(class_names[i]);
+    }
+  }
+
+  function toggle_record(event)
+  {
+    var record = event.currentTarget.parentNode;
+
+    // If item was not the previously selected item
+    if(!record.classList.contains(selected_item_class))
+    {
+      deselect_record(selected_record);
+      select_record(record);
+    }else
+    {
+      deselect_record(selected_record);
+    }
+  }
+
+  function select_record(record)
+  {
+    if(record)
+    {
+      // Set selected item to current item
+      selected_record = record;
+
+      // Add selected class
+      add_class(selected_record,selected_item_class);
+
+      // Show details
+      remove_class(
+        selected_record.querySelector(detail_query_class)
+        ,detail_hidden_class
+      );
+    }
+  }
+
+  function deselect_record(record)
+  {
+    if(
+      record
+      && record.classList.contains(selected_item_class)
+    )
+    {
+      // Remove selected class
+      remove_class(record,selected_item_class);
+
+      // Hide details
+      add_class(
+        record.querySelector(detail_query_class)
+        ,detail_hidden_class
+      );
+
+      // Clear selected item
+      selected_record = null;
     }
   }
 
@@ -153,80 +324,25 @@ app.start = function(records)
         ;
 
     header_row.appendChild(header_cell);
-    table.classList.add('detail_table');
+    add_class(table,'detail_table');
+
     if(class_name && class_name != '')
     {
-      table.classList.add(class_name);
+      add_class(table,class_name);
     }
+
     table.setAttribute('cellpadding',0);
     table.setAttribute('cellspacing',0);
-    header_cell.classList.add('detail_table_cell__header');
+    add_class(header_cell,'detail_table_cell__header');
     header_cell.textContent = header_text;
-    value_cell.classList.add('detail_table_cell__value')
+    add_class(value_cell,'detail_table_cell__value')
     value_cell.textContent = value;
 
     return table;
   }
 
-  function select_list_item(event)
-  {
-    var list_item            = event.currentTarget.parentNode
-        ,selected_item_class = 'list_item__selected'
-        ,detail_query_class  = '.list_item_container__detail'
-        ,detail_hidden_class = 'list_item_container__detail__hidden'
-        ;
-
-    // If item was previously selected (selected twice)
-    if(list_item.classList.contains(selected_item_class))
-    {
-      // Remove selected class
-      list_item
-        .classList
-        .remove(selected_item_class);
-
-      // Hide details
-      list_item
-        .querySelector(detail_query_class)
-        .classList
-        .add(detail_hidden_class);
-    }else
-    {
-      // If selected item exists
-      if(selected_list_item)
-      {
-        // Remove selected class
-        selected_list_item
-          .classList
-          .remove(selected_item_class)
-
-        // Hide details
-        selected_list_item
-          .querySelector(detail_query_class)
-          .classList
-          .add(detail_hidden_class)
-          ;
-      }
-
-      // Set selected item to current item
-      selected_list_item = list_item;
-
-      // Add selected class
-      selected_list_item
-        .classList
-        .add(selected_item_class)
-        ;
-
-      // Show details
-      selected_list_item
-        .querySelector(detail_query_class)
-        .classList
-        .remove(detail_hidden_class)
-        ;
-    }
-  }
-
   // BUILD RECORD LIST ITEMS
-  for(var i = 0,len = first_records.length;i < len;i++)
+  for(var i = 0,size = first_records.length;i < size;i++)
   {
     var dea_number          = first_records[i].dea_number
         ,npi                = first_records[i].npi
@@ -237,7 +353,7 @@ app.start = function(records)
         ,exp_day            = first_records[i].exp_date_parsed.getDate()
         ,exp_date_formatted = exp_month + ' ' + exp_day + ', ' + exp_year
         ,valid              = first_records[i].valid
-        ,list_item          = document.createElement('li')
+        ,record             = document.createElement('li')
         ,main_container     = document.createElement('div')
         ,avatar             = document.createElement('span')
         ,name_label         = document.createElement('span')
@@ -249,21 +365,33 @@ app.start = function(records)
         ,exp_date_table     = create_detail_table('Expiration Date',exp_date_formatted,'detail_table__expiration_date')
         ;
 
-    list_item.classList.add('list_item');
-    list_item.setAttribute('title',first_records[i].name);
-    if(!valid){list_item.classList.add('list_item__expired','hidden');}
+    add_class(record,'record');
+    record.setAttribute('title',first_records[i].name);
+
+    record_elements.push(record);
+
+    if(!valid)
+    {
+      add_class(record,'record__expired');
+      if(!expired_records_visible){hide(record);}
+      expired_records.push(record);
+    }else
+    {
+      valid_records.push(record);
+    }
 
     // MAIN CONTAINER
-    main_container.classList.add('list_item_container__main');
-    main_container.addEventListener('click',select_list_item,false);
+    add_class(main_container,'record_container__main');
+    main_container.addEventListener('click',toggle_record,false);
+
 
     // Avatar
-    avatar.classList.add('list_item_image__avatar');
+    add_class(avatar,'record_image__avatar');
     main_container.appendChild(avatar);
 
     // Name
     name_label.textContent = first_records[i].name;
-    name_label.classList.add('list_item_label__name');
+    add_class(name_label,'record_label__name');
     main_container.appendChild(name_label);
 
     // Expired
@@ -271,20 +399,22 @@ app.start = function(records)
 
     if(valid)
     {
-      expired_label.classList.add('hidden');
+      hide(expired_label);
     }else
     {
-      expired_label.classList.add('list_item_label__record_expired');
-      detail_container.classList.add('list_item_container__detail__expired');
+      add_class(expired_label,'record_label__record_expired');
+      add_class(detail_container,'record_container__detail__expired');
     }
 
     main_container.appendChild(expired_label);
-    list_item.appendChild(main_container);
+    record.appendChild(main_container);
 
     // DETAIL CONTAINER
-    detail_container
-      .classList
-      .add('list_item_container__detail','list_item_container__detail__hidden');
+    add_class(
+      detail_container
+      ,'record_container__detail'
+      ,'record_container__detail__hidden'
+    );
 
     detail_container.appendChild(dea_number_table);
     detail_container.appendChild(npi_table);
@@ -295,35 +425,44 @@ app.start = function(records)
     npi_tables.push(npi_table);
     provider_id_tables.push(provider_id_table);
 
-    list_item.appendChild(detail_container);
+    record.appendChild(detail_container);
 
-    list_item_container.appendChild(list_item);
+    record_container.appendChild(record);
   }
 
-  record_list.appendChild(list_item_container);
+  conceal(no_records_message);
+  record_list.appendChild(record_container);
+  reveal(record_list);
 
   // EVENTS
   // Toggle theme
   theme_toggle_btn.addEventListener('click',toggle_theme,false);
 
-  // Toggle record details
+  // Toggle record detail properties
   dea_number_toggle.addEventListener('change',function(event)
   {
-    batch_toggle_class_name(dea_number_tables,'hidden');
+    batch_toggle_class_name(dea_number_tables,hidden_class);
   },false);
 
   npi_toggle.addEventListener('change',function(event)
   {
-    batch_toggle_class_name(npi_tables,'hidden');
+    batch_toggle_class_name(npi_tables,hidden_class);
   },false);
 
   provider_id_toggle.addEventListener('change',function(event)
   {
-    batch_toggle_class_name(provider_id_tables,'hidden');
+    batch_toggle_class_name(provider_id_tables,hidden_class);
   },false);
 
   // Toggle expired records filter
-  filter_expired_btn.addEventListener('click',toggle_expired,false);
+  filter_expired_btn.addEventListener('click',toggle_expired_records,false);
+
+  // Filter records
+  record_filter.addEventListener(
+    'input'
+    ,record_filter_value_changed
+    ,{once:true,capture:false}
+  );
 
   // ----------------
   // END CONFIGURE UI
